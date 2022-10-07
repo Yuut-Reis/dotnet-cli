32b92b378a9f5f72c39cb8b6d07e48a567b2fef7
Yuut <ingride.reis.b@gmail.com>
2022-10-07 17:21:29 -0300

    feat(consoleProject): :sparkles: Function to print a message
    Function that returns Hello, .NET Core CLI!

--- src/HelloDotnetCLI/Program.cs
+++ src/HelloDotnetCLI/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, .NET Core CLI!");
+    }
+}
